dc02b1a1b6e5fcd9991ea330cbd5c0fde2f4f495
Ryan Guarino <rguarino@wesleyan.edu>
2017-07-08 15:46:29 -0400

    Initial Commit

---
+++ happyEars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  happyEars
+//
+//  Created by Ryan Guarino on 7/8/17.
+//  Copyright © 2017 Lilac LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
