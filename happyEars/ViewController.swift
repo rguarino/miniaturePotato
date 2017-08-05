d25a2261777caa3a98f0f8f3d7e35fbda30116b3
James <jamesramato@aol.com>
2017-08-05 12:42:46 -0400

    Progress Bar and Decibel Level views

--- happyEars/ViewController.swift
+++ happyEars/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import UICircularProgressRing
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressRing: UICircularProgressRingView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
