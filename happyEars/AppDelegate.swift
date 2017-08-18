f9a9a5f44b1c5d26ebb14ebf627cb797c820186f
Ryan Guarino <rguarino@wesleyan.edu>
2017-08-18 12:36:48 -0400

    uh uh

--- happyEars/AppDelegate.swift
+++ happyEars/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        func customUIBar() {
+            UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: UIFont(name: "Chalkduster", size: 23)!]
+        }
+        
+        customUIBar()
+        
         return true
     }
 
